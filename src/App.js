cca2f3fa2a6557981f6ce11a87a1f467b34c0936
yathansh13 <yathansh1312@gmail.com>
2022-09-17 18:28:54 +0530

    Minor tweaks

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import chat from "./Assets/chats.svg";
 import dots from "./Assets/dots.svg";
 import { useRef } from "react";
 import lgbt from "./Assets/diverse.svg";
-
+import bdots from "./Assets/bdots.svg";
+import wdots from "./Assets/wdots.svg";
+import gathering from "./Assets/laughter.svg";
+import stars from "./Assets/stars.svg";
 import Foorter from "./Components/Footer/Footer";
 
 function App() {
@@ -89,6 +92,44 @@ function App() {
           <Link style={{ textDecoration: "none" }} to="/Chat">
             <a className="Home_mid_button1">Start chatting</a>
           </Link>
+        </div>
+      </div>
+      <div className="yellow">
+        <div className="yellow_left">
+          <img className="yellow_dots" src={wdots} />
+          <img className="yellow_dots2" src={bdots} />
+
+          <img className="yellow_img" src={gathering} />
+        </div>
+        <div className="yellow_right">
+          <p className="yellow_right_header">
+            Perfect Place for meeting new people.
+          </p>
+          <p className="yellow_right_desc">
+            Find of diverse profiles from all over the world.
+          </p>
+          <div className="yellow_right_button">
+            <Link style={{ textDecoration: "none" }} to="/Chat">
+              <a className="yellow_right_button1"> Chats </a>
+            </Link>
+            <a className="yellow_right_button2"> Profile </a>
+          </div>
+          <svg
+            width="167"
+            height="1"
+            viewBox="0 0 167 1"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line y1="0.5" x2="167" y2="0.5" stroke="black" />
+          </svg>
+          <div className="review">
+            <img src={stars} className="review_stars" />
+            <p className="review_desc">
+              <span className="bold">5,200 users</span> use diversea and they
+              rate it as <span className="bold">5-stars</span>
+            </p>
+          </div>
         </div>
       </div>
 
